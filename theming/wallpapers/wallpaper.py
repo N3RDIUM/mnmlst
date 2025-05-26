03843382d473b6438f02b1f476525b5a18325062
n3rdium <n3rdium@gmail.com>
2025-05-26 09:29:48 +0530

    Time-based wallpaper

--- theming/wallpapers/wallpaper.py
+++ theming/wallpapers/wallpaper.py
@@ -1,11 +1,47 @@
 import os
-import random
+from datetime import datetime
+from time import sleep
 import subprocess
 
-wallpapers = os.listdir(os.path.dirname(__file__))
-wallpapers.remove("wallpaper.py")
+# Ensure single instance
+try:
+    with open(".circadian_lastpid") as f:
+        pid = f.read()
+    os.system(f"kill {pid}")
+except FileNotFoundError:
+    pass
+
+with open(".circadian_lastpid", "w") as f:
+    f.write(str(os.getpid()))
+
 
 _ = subprocess.call("swww-daemon")
-_ = os.system(
-    f"swww img {os.path.join(os.path.dirname(__file__), random.choice(wallpapers))}"
-)
+
+def get_hour() -> int:
+    return datetime.now().hour
+
+def set_wall(name):
+    _ = os.system(
+        f"swww img {os.path.join(os.path.dirname(__file__), name)}"
+    )
+
+DAY = "von.jpg"
+NIGHT = "pxl.jpg"
+
+previous = None
+current = NIGHT
+
+while True:
+    hour = get_hour()
+
+    if hour >= 6 and hour <= 18:
+        current = DAY
+    else:
+        current = NIGHT
+
+    if current != previous:
+        set_wall(current)
+        previous = current
+
+    sleep(20)
+
