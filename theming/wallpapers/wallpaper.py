04459792469b6f526d672ae088db80055e68d128
n3rdium <n3rdium@gmail.com>
2025-07-05 12:25:01 +0530

    Wallpaper

--- theming/wallpapers/wallpaper.py
+++ theming/wallpapers/wallpaper.py
@@ -1,19 +1,5 @@
 import os
-from datetime import datetime
-from time import sleep
 import subprocess
-
-# Ensure single instance
-try:
-    with open(".wallsetter_lastpid") as f:
-        pid = f.read()
-    os.system(f"kill {pid}")
-except FileNotFoundError:
-    pass
-
-with open(".wallsetter_lastpid", "w") as f:
-    f.write(str(os.getpid()))
-
 
 _ = subprocess.call("swww-daemon")
 
@@ -22,6 +8,5 @@ def set_wall(name):
         f"swww img {os.path.join(os.path.dirname(__file__), name)}"
     )
 
-while True:
-    set_wall("classroom.jpg")
+set_wall("classroom.jpg")
 
