7c68281b7bda8c212bbebbbfa1e07f10a9b70cf5
n3rdium <n3rdium@gmail.com>
2025-07-05 09:38:54 +0530

    OSD

--- shell/app.ts
+++ shell/app.ts
@@ -1,6 +1,14 @@
 import app from "ags/gtk4/app"
 import theme from "./index.scss"
-import { Osd } from "./widget/Osd"
+import { Osd, OsdRequestHandler } from "./widget/Osd"
+
+function parseRequest(req: String) {
+    return req.toString().split(" ")
+}
+
+const requestMap = {
+    osd: OsdRequestHandler
+};
 
 app.start({
     css: theme,
@@ -9,7 +17,13 @@ app.start({
         Osd(main_monitor);
     },
     requestHandler(request, res) {
-        console.log(request);
-        res("ok");
+        let req = parseRequest(request);
+        let to = req[0];
+        let body = req.slice(1, req.length);
+        if(requestMap[to] == undefined) {
+            res("unknown request");
+            return;
+        };
+        requestMap[to](body, res);
     }
 })
