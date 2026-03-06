f30888ab2357212f4c59173a08680d05825e681f
n3rdium <n3rdium@gmail.com>
2026-03-06 23:21:05 +0530

    fix: second_monitor conf

--- second_monitor/configuration.nix
+++ second_monitor/configuration.nix
@@ -149,20 +149,11 @@
         group = "git";
         home = "/home/git";
         createHome = true;
-        shell = "${pkgs.git}/bin/git-shell";
 
         openssh.authorizedKeys.keys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB+OyIQBrmUL0No1td+0lZytP+Kak3WMizPC7pCNTbSt n3rdium"
         ];
     };
-
-    services.openssh.extraConfig = ''
-        Match User git
-            AllowTcpForwarding no
-            AllowAgentForwarding no
-            PasswordAuthentication no
-            X11Forwarding no
-    '';
 
     # env pkgs
     environment.systemPackages = with pkgs; [
