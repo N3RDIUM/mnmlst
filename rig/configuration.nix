cfd9a288639f166e1cc9ff5901c1dbc03e4f7d3e
n3rdium <n3rdium@gmail.com>
2026-03-09 23:13:11 +0530

    fix: minor refactors

--- rig/configuration.nix
+++ rig/configuration.nix
@@ -9,10 +9,12 @@
         ./config/automount.nix
         ./config/boot.nix
         ./config/cli.nix
+        ./config/env.nix
         ./config/essentials.nix
         ./config/input.nix
+        ./config/users.nix
         ./config/networking.nix
-        ./config/virtualization.nix
+        # ./config/virtualization.nix
         ./config/wm.nix
 
         ./packages/dev.nix
@@ -26,44 +28,6 @@
         trusted-substituters = ["https://hyprland.cachix.org"];
         trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
-
-	users.users = {
-		n3rdium = {
-			isNormalUser = true;
-			description	= "n3rdium";
-            shell = pkgs.fish;
-			extraGroups	= [ "networkmanager" "wheel" "audio" "jackaudio" "dialout" "uucp" "uinput" "i2c" ];
-		};
-
-		not-n3rdium = {
-			isNormalUser = true;
-			description	= "not-n3rdium";
-            shell = pkgs.fish;
-			extraGroups	= [ "networkmanager" ];
-		};
-	};
-
-    environment.variables =
-        let
-            makePluginPath = format:
-            (lib.makeSearchPath format [
-                "$HOME/.nix-profile/lib"
-                "/run/current-system/sw/lib"
-                "/etc/profiles/per-user/$USER/lib"
-            ])
-            + ":$HOME/.${format}";
-        in
-        {
-            DSSI_PATH = makePluginPath "dssi";
-            LADSPA_PATH = makePluginPath "ladspa";
-            LV2_PATH = makePluginPath "lv2";
-            LXVST_PATH = makePluginPath "lxvst";
-            VST_PATH = makePluginPath "vst";
-            VST3_PATH = makePluginPath "vst3";
-            ROC_ENABLE_PRE_VEGA = "1";
-            RUSTICL_ENABLE = "radeonsi";
-            EDITOR = "nvim";
-        };
 
 	system.stateVersion = "24.11";
 }
