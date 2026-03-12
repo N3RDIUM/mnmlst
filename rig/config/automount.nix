dae39d0643a5b5a823c02d5e99e60530102b8b99
n3rdium <n3rdium@gmail.com>
2026-03-12 18:25:33 +0530

    fix: rename mountpoints

--- rig/config/automount.nix
+++ rig/config/automount.nix
@@ -1,11 +1,11 @@
 { inputs, config, pkgs, lib, ... }:
 {
 	boot.supportedFilesystems = [ "ntfs" ];
-	fileSystems."/mnt/Code"	 = {
+	fileSystems."/mnt/code"	 = {
 		device = "/dev/disk/by-uuid/b95320b3-3df1-4904-a55e-da1e8d819231";
 		options = [ "x-systemd.automount" "x-systemd.device-timeout=0" "nofail" ];
 	};
-	fileSystems."/mnt/Space"	= {
+	fileSystems."/mnt/space"	= {
 		device = "/dev/disk/by-uuid/0A956B927E2FFFE8";
 		fsType = "ntfs-3g";
 		options = [ "x-systemd.automount" "x-systemd.device-timeout=0" "rw" "uid=1000" "nofail" ];
