0655f0e85a4bd2b41d9d94adc1fc940c74e8042c
n3rdium <n3rdium@gmail.com>
2026-03-12 16:06:07 +0530

    fix: networking setup

--- lap/config/networking.nix
+++ lap/config/networking.nix
@@ -25,9 +25,9 @@
     # Networking
     networking.networkmanager.enable = true;
     networking.hostName = "n3rdium-lap";
-    networking.interfaces.eno1.ipv4.addresses = [
-        { address = "192.168.1.69"; prefixLength = 24; }
-    ];
+    # networking.interfaces.eno1.ipv4.addresses = [
+    #   { address = "192.168.1.69"; prefixLength = 24; }
+    # ];
     networking.defaultGateway = "192.168.1.1";
     networking.nameservers = [  "1.1.1.1" "8.8.8.8" ];
     networking.firewall.allowedTCPPorts = [ 22 8001 ];
