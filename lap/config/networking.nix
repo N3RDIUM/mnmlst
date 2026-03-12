3428937e6134e132afbfcb132fe1dbbcdf2eee6a
n3rdium <n3rdium@gmail.com>
2026-03-12 16:08:30 +0530

    fix: networking setup

--- lap/config/networking.nix
+++ lap/config/networking.nix
@@ -17,17 +17,32 @@
 
     # wol
     networking = {
+        interfaces = {
+            enp1s0 = {
+                wakeOnLan.enable = true;
+            };
+        };
         firewall = {
             allowedUDPPorts = [ 9 8001 ];
+        };
+    };
+
+    systemd.services.wakeonlan = {
+        description = "Enable Wake-on-LAN (WoL)";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+            Type = "oneshot";
+            ExecStart = "${pkgs.ethtool}/bin/ethtool -s enp1s0 wol g";
         };
     };
 
     # Networking
     networking.networkmanager.enable = true;
     networking.hostName = "n3rdium-lap";
-    # networking.interfaces.eno1.ipv4.addresses = [
-    #   { address = "192.168.1.69"; prefixLength = 24; }
-    # ];
+    networking.interfaces.enp1s0.ipv4.addresses = [
+        { address = "192.168.1.69"; prefixLength = 24; }
+    ];
     networking.defaultGateway = "192.168.1.1";
     networking.nameservers = [  "1.1.1.1" "8.8.8.8" ];
     networking.firewall.allowedTCPPorts = [ 22 8001 ];
